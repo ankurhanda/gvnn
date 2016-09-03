f442fd0ced96bc5d5b84a53926c544b94d44277f
Ankur Handa <handa.ankur@gmail.com>
2016-09-03 15:38:51 +0100

    renaming file
    Signed-off-by: Ankur Handa <handa.ankur@gmail.com>

---
+++ imagewarpingSO3.lua
@@ -0,0 +1,33 @@
+require 'nn'
+require 'gvnn'
+
+concat = nn.ConcatTable()
+
+height = 240
+width  = 320
+u0     = 160
+v0     = 120
+
+fx = 240
+fy = 240
+
+-- first branch is there to transpose inputs to BHWD, for the bilinear sampler
+tranet=nn.Sequential()
+tranet:add(nn.SelectTable(1))
+tranet:add(nn.Identity())
+tranet:add(nn.Transpose({2,3},{3,4}))
+
+rotation_net = nn.Sequential()
+rotation_net:add(nn.SelectTable(2))
+rotation_net:add(nn.TransformationRotationSO3())
+rotation_net:add(nn.Transform3DPoints_R(height, width, fx, fy, u0, v0))
+rotation_net:add(nn.PinHoleCameraProjectionBHWD(height, width, fx, fy, u0, v0))
+rotation_net:add(nn.ReverseXYOrder())
+
+concat:add(tranet)
+concat:add(rotation_net)
+
+warping_net = nn.Sequential()
+warping_net:add(concat)
+warping_net:add(nn.BilinearSamplerBHWD())
+warping_net:add(nn.Transpose({3,4},{2,3}))
